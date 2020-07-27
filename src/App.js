fa699e5d0b157c00774e23a9a418a96177d00d73
mohsinghani <mohsinghani.777@gmail.com>
2020-07-27 16:35:23 +0500

    scroll-lazy-loading

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import List from './Components/list/list'
+function App() {
+  return (
+    <div className="App">
+      <List/>
+    </div>
+  );
+}
+
+export default App;
